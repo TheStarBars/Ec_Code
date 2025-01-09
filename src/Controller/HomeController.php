db38e461711fa89137bce1cf8df7ffc87dcf8967
TheStarBars <aoges@orange.fr>
2025-01-09 11:13:34 +0100

    first commit

---
+++ src/Controller/HomeController.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Controller;
+
+use App\Repository\BookReadRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+class HomeController extends AbstractController
+{
+    private BookReadRepository $readBookRepository;
+
+    // Inject the repository via the constructor
+    public function __construct(BookReadRepository $bookReadRepository)
+    {
+        $this->bookReadRepository = $bookReadRepository;
+    }
+
+    #[Route('/', name: 'app.home')]
+    public function index(): Response
+    {
+        $userId     = 1;
+        $booksRead  = $this->bookReadRepository->findByUserId($userId, false);
+
+        // Render the 'hello.html.twig' template
+        return $this->render('pages/home.html.twig', [
+            'booksRead' => $booksRead,
+            'name'      => 'Accueil', // Pass data to the view
+        ]);
+    }
+}
